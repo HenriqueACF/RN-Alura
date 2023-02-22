a7bc4636b5194e738371d02391336b3710e44bf8
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-02-22 16:39:18 -0300

    style: tab navigator

--- RN-Alura2/src/routes/AppRoutes.js
+++ RN-Alura2/src/routes/AppRoutes.js
@@ -5,6 +5,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 //COMPONENTS
 import ProdutorRoutes from './ProdutorRoutes';
 import MelhoresProdutoresRoutes from './MelhoresProdutoresRoutes';
+// ICONS
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,9 +15,20 @@ export default function AppRoutes() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({route}) => ({
           headerShown: false,
-        }}>
+          tabBarActiveTintColor: '#2a9f85',
+          tabBarInactiveTintColor: '#c7c7c7',
+          tabBarIcon: ({color}) => {
+            let Icon = Home;
+
+            if (route.name === 'Melhores Produtores') {
+              Icon = Coracao;
+            }
+
+            return <Icon color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={ProdutorRoutes} />
         <Tab.Screen
           name="Melhores Produtores"
